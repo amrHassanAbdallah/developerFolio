012abb86f460fb9cdc71c8ccb826d3a7630a6987
Amr Hassan <amr.hassan.abdullah@gmail.com>
2022-11-23 16:09:05 +0200

    chore: updated the page title when opening the resume.

--- src/pages/resume/Resume.js
+++ src/pages/resume/Resume.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React, {useEffect} from "react";
 import "./Resume.scss";
 
-
 export default function Resume() {
+    useEffect(() => {
+        document.title = 'Amr Hassan | Resume';
+    }, []);
+
   return (
       <div className="iframe-pdf">
         <iframe title="resume" src="./resume.pdf"></iframe>

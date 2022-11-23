935d3cf43d00f576f5cb6c3d15da97822c1312fd
Amr Hassan <amr.hassan.abdullah@gmail.com>
2022-11-23 06:43:31 +0200

    chore: displaying pdf as page.

--- src/App.js
+++ src/App.js
@@ -2,19 +2,19 @@ import React from "react";
 import "./App.scss";
 import "./assets/fonts/times new roman italic.ttf";
 import Main from "./containers/Main";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {HashRouter, Route, Routes} from "react-router-dom";
 import Resume from "./pages/resume/Resume";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
         <Routes>
             <Route path="/">
                 <Route index element={<Main />} />
                 <Route path="/resume" element={<Resume />} />
             </Route>
         </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
